27ced0580ab903368b7ce9f8a1675410cbf8326d
Karoline Suzuki <42789906+suzukikarol@users.noreply.github.com>
2018-11-06 21:58:09 -0200

    Add files via upload

---
+++ BibliotecaVirtual/BibliotecaVirtual/BibliotecaContexto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual
+{
+    class BibliotecaContexto : DbContext, IDisposable
+    {
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Emprestimo>Emprestimos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BibliotecaVirtual;Trusted_Connection=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//executado na criaçao do modelo
+        {
+
+            modelBuilder
+                .Entity<Livro>()
+                .HasKey(pp => new { pp.Id});
+
+            modelBuilder
+                .Entity<Usuario>()
+                .HasKey(pp => new { pp.Id});
+
+            base.OnModelCreating(modelBuilder);
+
+            //////////////////////////////////////
+        }
+    }
+}
+
